7e15adcf0c4e1757df272ff6d25a1413a83be4a5
Himanshu Negi <himanshu.negi@curefit.com>
2021-07-21 16:41:08 +0530

    initialize front camera on launch

--- barcode-reader/src/main/java/info/androidhive/barcode/BarcodeReader.java
+++ barcode-reader/src/main/java/info/androidhive/barcode/BarcodeReader.java
@@ -248,7 +248,7 @@ public class BarcodeReader extends Fragment implements View.OnTouchListener, Bar
         // to other detection examples to enable the barcode detector to detect small barcodes
         // at long distances.
         CameraSource.Builder builder = new CameraSource.Builder(getActivity(), barcodeDetector)
-                .setFacing(CameraSource.CAMERA_FACING_BACK)
+                .setFacing(CameraSource.CAMERA_FACING_FRONT)
                 .setRequestedPreviewSize(1600, 1024)
                 .setRequestedFps(15.0f);
 
